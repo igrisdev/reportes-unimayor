24c638748b22a971dd494c41720ff0e0142ed923
igrisdev <johanalvarezz237@gmail.com>
2025-05-24 14:47:27 -0500

    remove explore container and tab components

--- src/app/tabs/tabs.routes.ts
+++ src/app/tabs/tabs.routes.ts
@@ -3,24 +3,14 @@ import { TabsPage } from './tabs.page';
 
 export const routes: Routes = [
   {
-    path: 'tabs',
+    path: '',
     component: TabsPage,
     children: [
-      {
-        path: 'tab1',
-        loadComponent: () =>
-          import('../tab1/tab1.page').then((m) => m.Tab1Page),
-      },
-      {
-        path: 'tab2',
-        loadComponent: () =>
-          import('../tab2/tab2.page').then((m) => m.Tab2Page),
-      },
-      {
-        path: 'tab3',
-        loadComponent: () =>
-          import('../tab3/tab3.page').then((m) => m.Tab3Page),
-      },
+      // {
+      //   path: 'tab3',
+      //   loadComponent: () =>
+      //     import('../tab3/tab3.page').then((m) => m.Tab3Page),
+      // },
       {
         path: '',
         redirectTo: '/tabs/tab1',
@@ -30,7 +20,7 @@ export const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs',
     pathMatch: 'full',
   },
 ];
