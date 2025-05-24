77c2c67cdd2befda9145c115f156f6f4d7946f2e
igrisdev <johanalvarezz237@gmail.com>
2025-05-24 15:07:10 -0500

    refactor: update tab routing and remove hardcoded tab links

--- src/app/pages/tabs/tabs.routes.ts
+++ src/app/pages/tabs/tabs.routes.ts
@@ -6,41 +6,45 @@ export const routes: Routes = [
     path: '',
     component: TabsPage,
     children: [
-      // {
-      //   path: 'tab3',
-      //   loadComponent: () =>
-      //     import('../tab3/tab3.page').then((m) => m.Tab3Page),
-      // },
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('./user/home/home.page').then((m) => m.HomePage),
+      },
+      {
+        path: 'history',
+        loadComponent: () =>
+          import('./user/history/history.page').then((m) => m.HistoryPage),
+      },
+      {
+        path: 'profile',
+        loadComponent: () =>
+          import('./user/profile/profile.page').then((m) => m.ProfilePage),
+      },
+      {
+        path: 'make-report',
+        loadComponent: () =>
+          import('./user/make-report/make-report.page').then(
+            (m) => m.MakeReportPage
+          ),
+      },
+      {
+        path: 'view-report',
+        loadComponent: () =>
+          import('./user/view-report/view-report.page').then(
+            (m) => m.ViewReportPage
+          ),
+      },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/home',
         pathMatch: 'full',
       },
     ],
   },
   {
     path: '',
-    redirectTo: '/tabs',
+    redirectTo: '/home',
     pathMatch: 'full',
-  },  {
-    path: 'home',
-    loadComponent: () => import('./user/home/home.page').then( m => m.HomePage)
   },
-  {
-    path: 'history',
-    loadComponent: () => import('./user/history/history.page').then( m => m.HistoryPage)
-  },
-  {
-    path: 'profile',
-    loadComponent: () => import('./user/profile/profile.page').then( m => m.ProfilePage)
-  },
-  {
-    path: 'make-report',
-    loadComponent: () => import('./user/make-report/make-report.page').then( m => m.MakeReportPage)
-  },
-  {
-    path: 'view-report',
-    loadComponent: () => import('./user/view-report/view-report.page').then( m => m.ViewReportPage)
-  },
-
 ];
