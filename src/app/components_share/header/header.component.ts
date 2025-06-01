c7c79df9ec69dddebfb38c90c344967f7226a47d
igrisdev <johanalvarezz237@gmail.com>
2025-06-01 18:05:53 -0500

    refactor: improve header component logic and styling; enhance role handling and routing

--- src/app/components_share/header/header.component.ts
+++ src/app/components_share/header/header.component.ts
@@ -1,36 +1,59 @@
-import { Component, OnInit, signal } from '@angular/core';
-import { IonHeader, IonToolbar } from '@ionic/angular/standalone';
+import { Component, inject, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+  IonHeader,
+  IonToolbar,
+  IonRouterLink,
+} from '@ionic/angular/standalone';
 import { decodeJWT } from 'src/app/guard/login.guard';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  imports: [IonToolbar, IonHeader],
+  imports: [IonRouterLink, IonToolbar, IonHeader],
 })
-export class HeaderComponent implements OnInit {
-  brigadier = signal<boolean>(false);
+export class HeaderComponent {
+  router = inject(Router);
 
+  isBrigadier = signal<boolean>(false);
   rol = signal<string>('');
 
   constructor() {
-    this.isBrigadier();
+    this.handleRol();
   }
 
-  ngOnInit() {}
-
-  isBrigadier() {
+  handleRol() {
     const token = localStorage.getItem('token');
-    if (!token) return;
-
     const decoded = decodeJWT(token!);
 
-    if (decoded.EsBrigadista == true) {
-      this.rol.set('brigadier');
+    const path = this.getPath();
+
+    if (path === 'brigadier') {
+      this.rol.set('Brigadier');
     } else {
-      this.rol.set('usuario');
+      this.rol.set('Usuario');
     }
 
-    return this.brigadier.set(decoded.EsBrigadista);
+    const isRol = decoded.EsBrigadista === 'true' ? true : false;
+
+    this.isBrigadier.set(isRol);
+  }
+
+  getPath() {
+    const path = this.router.url.split('/')[1];
+    return path;
+  }
+
+  handleRedirect() {
+    const path = this.getPath();
+
+    if (path === 'brigadier') {
+      this.router.navigate(['/user']);
+      return;
+    }
+
+    this.router.navigate([`/brigadier`]);
+    return;
   }
 }
