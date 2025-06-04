283666ea7569a44dec86da0db319e5d22cd16886
igrisdev <johanalvarezz237@gmail.com>
2025-06-04 15:34:56 -0500

    1

--- src/app/pages/tabs/user/make-report/make-report.page.ts
+++ src/app/pages/tabs/user/make-report/make-report.page.ts
@@ -1,9 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
-import { IonContent, IonRouterLink, IonIcon } from '@ionic/angular/standalone';
+import { IonContent, IonRouterLink, IonIcon, IonSpinner } from '@ionic/angular/standalone';
 import { HeaderComponent } from '../../../../components_share/header/header.component';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { addIcons } from 'ionicons';
 import { arrowBackOutline } from 'ionicons/icons';
 import { LocationService } from 'src/app/service/location/location.service';
@@ -22,24 +22,42 @@ import { ReportsService } from 'src/app/service/reports.service';
     IonContent,
     HeaderComponent,
     RouterLink,
+    IonSpinner,
   ],
 })
 export class MakeReportPage {
   private fb = inject(FormBuilder);
   private reportsService = inject(ReportsService);
+  private router = inject(Router);
 
   readonly locations = inject(LocationService).getLocations();
+  readonly isSubmitting = signal(false);
 
   form = this.fb.group({
     location: ['', [Validators.required]],
     description: ['', [Validators.required]],
   });
 
-  handleSubmit() {
-    this.reportsService.createReport(
-      Number(this.form.value.location),
-      this.form.value.description!
-    );
+  async handleSubmit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    this.isSubmitting.set(true);
+
+    try {
+      this.reportsService.createReport(
+        Number(this.form.value.location),
+        this.form.value.description!
+      );
+      this.router.navigate(['/user/home']); // Corrected path
+    } catch (error) {
+      console.error('Error creating report:', error);
+      // Optionally, show an error message to the user
+    } finally {
+      this.isSubmitting.set(false);
+    }
   }
 
   constructor() {
