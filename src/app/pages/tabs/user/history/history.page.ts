62807b5feaf462794da1591da1fac3581c32eaf9
igrisdev <johanalvarezz237@gmail.com>
2025-06-05 16:15:02 -0500

    refactor: update HistoryPage to use ReportService and implement signal for report management

--- src/app/pages/tabs/user/history/history.page.ts
+++ src/app/pages/tabs/user/history/history.page.ts
@@ -1,4 +1,4 @@
-import { Component, effect, inject, OnInit } from '@angular/core';
+import { Component, effect, inject, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonContent } from '@ionic/angular/standalone';
 import { ReportsService } from 'src/app/service/reports.service';
@@ -6,6 +6,7 @@ import { NoReportsComponent } from '../../../../widget/no-reports/no-reports.com
 import { HeaderComponent } from '../../../../components_share/header/header.component';
 import { CardStatusComponent } from '../../../../components_share/card-status/card-status.component';
 import { LinkCreateReportComponent } from '../../../../widget/link-create-report/link-create-report.component';
+import { ReportService } from 'src/app/service/report/report.service';
 
 @Component({
   selector: 'app-history',
@@ -22,13 +23,31 @@ import { LinkCreateReportComponent } from '../../../../widget/link-create-report
   ],
 })
 export class HistoryPage implements OnInit {
-  private reportService = inject(ReportsService);
+  private reportsService = inject(ReportService);
+  reports = signal<any>([]);
 
-  reports: any = [];
+  constructor() {}
 
-  constructor() {
-    this.reports = this.reportService.getReports();
+  ngOnInit() {
+    this.loadReports();
   }
 
-  ngOnInit() {}
+  // ionViewWillEnter() {
+  //   this.loadReports();
+  // }
+
+  private async loadReports() {
+    this.reportsService.getAllReports().subscribe({
+      next: (data: any) => {
+        const sortReports = data.sort((a: any, b: any) => {
+          return a.fechaCreacion > b.fechaCreacion ? -1 : 1;
+        });
+
+        this.reports.set(sortReports);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
