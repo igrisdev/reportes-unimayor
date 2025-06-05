0b30b0f2a866308d698352c1217acbcd76484266
igrisdev <johanalvarezz237@gmail.com>
2025-06-05 16:37:08 -0500

    refactor: simplify form submission in MakeReportPage and remove unused spinner

--- src/app/pages/tabs/user/make-report/make-report.page.ts
+++ src/app/pages/tabs/user/make-report/make-report.page.ts
@@ -1,13 +1,13 @@
 import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
-import { IonContent, IonRouterLink, IonIcon, IonSpinner } from '@ionic/angular/standalone';
+import { IonContent, IonRouterLink, IonIcon } from '@ionic/angular/standalone';
 import { HeaderComponent } from '../../../../components_share/header/header.component';
 import { Router, RouterLink } from '@angular/router';
 import { addIcons } from 'ionicons';
 import { arrowBackOutline } from 'ionicons/icons';
 import { LocationService } from 'src/app/service/location/location.service';
-import { ReportsService } from 'src/app/service/reports.service';
+import { ReportService } from 'src/app/service/report/report.service';
 
 @Component({
   selector: 'app-make-report',
@@ -22,47 +22,42 @@ import { ReportsService } from 'src/app/service/reports.service';
     IonContent,
     HeaderComponent,
     RouterLink,
-    IonSpinner,
   ],
 })
 export class MakeReportPage {
   private fb = inject(FormBuilder);
-  private reportsService = inject(ReportsService);
+  private reportService = inject(ReportService);
   private router = inject(Router);
 
   readonly locations = inject(LocationService).getLocations();
-  readonly isSubmitting = signal(false);
 
   form = this.fb.group({
     location: ['', [Validators.required]],
     description: ['', [Validators.required]],
   });
 
-  async handleSubmit() {
-    if (this.form.invalid) {
-      this.form.markAllAsTouched();
-      return;
-    }
-
-    this.isSubmitting.set(true);
-
-    try {
-      this.reportsService.createReport(
-        Number(this.form.value.location),
-        this.form.value.description!
-      );
-      this.router.navigate(['/user/home']); // Corrected path
-    } catch (error) {
-      console.error('Error creating report:', error);
-      // Optionally, show an error message to the user
-    } finally {
-      this.isSubmitting.set(false);
-    }
-  }
-
   constructor() {
     addIcons({
       arrowBackOutline,
     });
+  }
+
+  handleSubmit() {
+    this.reportService
+      .createReport(
+        Number(this.form.value.location),
+        this.form.value.description!
+      )
+      .subscribe({
+        next: (data: any) => {
+          console.log(data);
+          if (data.status === 201) {
+            // this.router.navigate(['/user/home']);
+          }
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
   }
 }
