a9d1c5d6055d409b95fef19732713614159778a8
igrisdev <johanalvarezz237@gmail.com>
2025-06-05 17:23:07 -0500

    feat: enhance ReportBrigadierService with new methods and update settings for spell check

--- src/app/service/report-brigadier/report-brigadier.service.ts
+++ src/app/service/report-brigadier/report-brigadier.service.ts
@@ -1,9 +1,41 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { BaseService } from '../base.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReportBrigadierService {
+  private _http = inject(BaseService);
+  private _url = environment.API_URL;
 
-  constructor() { }
+  getAllReportsBrigadier() {
+    const path = this._url + 'brigadista/reportes/pendientes';
+
+    return this._http.getWithToken(path);
+  }
+
+  getReportAcceptedBrigadier() {
+    const path = this._url + 'brigadista/reportes/asignados';
+
+    return this._http.getWithToken(path);
+  }
+
+  acceptedReport(id: number) {
+    const path = this._url + 'brigadista/reportes/aceptar/' + id;
+
+    return this._http.putWithToken(path, { estado: 'Aceptado' });
+  }
+
+  finalizeReport(id: number) {
+    const path = this._url + 'brigadista/reportes/finalizar/' + id;
+
+    return this._http.putWithToken(path, { estado: 'Finalizado' });
+  }
+
+  // getReport(id: number) {
+  //   const path = this._url + 'reportes/' + id;
+
+  //   return this._http.getWithToken(path);
+  // }
 }
